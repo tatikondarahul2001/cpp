306eed3875060cde1299f8cac7b3648acc803c03
Tatikonda Rahul <74859747+Rahulisunq@users.noreply.github.com>
2020-12-08 05:15:25 +0000

    Add files via upload

---
+++ 7122020.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+namespace tatikonda
+{
+ class A
+  {
+   public:int a=10;
+    void main()
+     {
+      cout<<"hello world !!!";
+     }
+  };
+}
+int main()
+{
+tatikonda::A O;
+O.main();
+}
